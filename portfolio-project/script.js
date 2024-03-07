ff8af5d48b0f90444c2ad4d23e658b43565b4baf
ahmed salhi <sirjaizz@gmail.com>
2024-03-07 15:06:54 +0100

    portoflio exercise

---
+++ portfolio-project/script.js
@@ -0,0 +1,8 @@
+<script>
+document.getElementById('slide-right').onclick = function () {
+    document.getElementById('project-slider').scrollLeft += 200;
+};
+document.getElementById('slide-left').onclick = function () {
+    document.getElementById('project-slider').scrollLeft -= 200;
+};
+</script>
