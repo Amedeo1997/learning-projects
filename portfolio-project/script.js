cdca2eee0ea1a697c9a55e541dc5f13399d9c65d
ahmed salhi <sirjaizz@gmail.com>
2024-03-07 15:08:28 +0100

    done

--- portfolio-project/script.js
+++
@@ -1,8 +0,0 @@
-<script>
-document.getElementById('slide-right').onclick = function () {
-    document.getElementById('project-slider').scrollLeft += 200;
-};
-document.getElementById('slide-left').onclick = function () {
-    document.getElementById('project-slider').scrollLeft -= 200;
-};
-</script>
