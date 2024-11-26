14ccd8735d543fa6313dcd7f070a3b54e09755a2
ahmed salhi <sirjaizz@gmail.com>
2024-11-26 15:27:50 +0100

    propreties odne

---
+++ learn-js/script.js
@@ -0,0 +1,27 @@
+let a = 10;
+let b = 10;
+
+//addizione
+console.log(a + b);
+//sottrazione
+console.log(a - b);
+//divisione
+console.log(a / b);
+//moltiplicazione
+console.log(a * b);
+//modulo
+console.log( a % b);
+
+const name = "Amedeo";
+let hello = "Ciao, mi chiamo " + name + "!";
+console.log(hello);
+hello = `Hello, mi chiamo ${name}!`;
+console.log(hello);
+
+let vero = true;
+let falso = false;
+let niente = null;
+let vuoto;
+let deciamle = 1.1;
+let intero = 1;
+console.log(vero, falso, niente, vuoto, deciamle, intero)
